2577e5bf2f1239b36d7156fc691b958975e8d9a5
user <user@DESKTOP-H19C2NI>
2021-05-18 17:25:37 +0300

    סיום תרגיל 6 לא כולל תיעודים מלאים

--- src/elements/PointLight.java
+++ src/elements/PointLight.java
@@ -4,11 +4,12 @@ import primitives.Color;
 import primitives.Point3D;
 import primitives.Vector;
 
-public class PointLight extends Light implements LightSource{
+public class PointLight extends Light implements LightSource {
     private final Point3D _position;
-    private  double _Kc = 1.0;
-    private  double _Kl=0;
-    private  double _Kq=0;
+    private double _Kc = 1.0;
+    private double _Kl = 0;
+    private double _Kq = 0;
+
     /**
      * constractor
      *
@@ -21,19 +22,17 @@ public class PointLight extends Light implements LightSource{
     }
 
     /**
-     *
      * @param p
      * @return
      */
     @Override
     public Color getIntensity(Point3D p) {
-        double d=_position.distance(p);
-        double attenuation=1d/(_Kc+_Kl*d+_Kq*d*d);
+        double d = _position.distance(p);
+        double attenuation = 1d / (_Kc + _Kl * d + _Kq * d * d);
         return _intensity.scale(attenuation);
     }
 
     /**
-     *
      * @param p
      * @return the light from position to p
      */
@@ -58,3 +57,5 @@ public class PointLight extends Light implements LightSource{
         return this;
     }
 }
+
+
